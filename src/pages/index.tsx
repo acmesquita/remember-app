5a7e36cf0abe5d2b17b2b213e7169160168c99de
Catharina Mesquita <catha.ana.1994@gmail.com>
2021-04-15 11:10:17 -0300

    feat: iniciando o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.title}>Próxima atividade</h1>
+      <p className={styles.time}>25:00</p>
+    </main>
+  )
+}
